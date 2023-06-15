fcf937d44a571ba5bee31c6707c65630bed4ed16
AakashR3 <109940526+AakashR3@users.noreply.github.com>
2023-06-15 10:57:00 +0530

    Update Filecopy.ps1

--- Filecopy.ps1
+++ Filecopy.ps1
@@ -1,5 +1,6 @@
 $session = New-PSSession -ComputerName 40.112.215.123 -Credential (Get-Credential -UserName "azureuser" -Password "codincity@123")
-$sourcePath = Join-Path $env:WORKSPACE "publish"
+$sourcePath = "$env:WORKSPACE/publish"
 $destinationPath = "C:\\Users\\azureuser\\Desktop\"
 Copy-Item -Path $sourcePath -Destination $destinationPath -ToSession $session
 Remove-PSSession -Session $session
+
