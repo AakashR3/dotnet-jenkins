38e7285c7f7fe3706d935a50e58df83327806606
AakashR3 <109940526+AakashR3@users.noreply.github.com>
2023-06-16 12:04:55 +0530

    Update Filecopy.ps1

--- Filecopy.ps1
+++ Filecopy.ps1
@@ -4,6 +4,10 @@ $secureCred = ConvertTo-SecureString $password -AsPlainText -Force
 $credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $userName, $secureCred
 $session = New-PSSession -ComputerName 40.112.215.123 -Credential $credential
 $sourcePath = "$env:WORKSPACE/publish"
-$destinationPath = "C:\\Users\\azureuser\\Desktop\"
-Copy-Item -Path $sourcePath -Destination $destinationPath -ToSession $session
+$destinationPath = "C:\Users\azureuser\Desktop\"
+Invoke-Command -Session $session -ArgumentList $sourcePath, $destinationPath -ErrorVariable err_return -ScriptBlock{
+#Copy-Item -Path $sourcePath -Destination $destinationPath
+Copy-Item -Path $args[0] -Destination $args[1]
+
+}
 Remove-PSSession -Session $session
