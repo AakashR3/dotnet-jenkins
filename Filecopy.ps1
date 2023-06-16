cd9ad2e46d44156ed25c465e82ebfd542417379a
AakashR3 <109940526+AakashR3@users.noreply.github.com>
2023-06-16 20:17:06 +0530

    Update Filecopy.ps1

--- Filecopy.ps1
+++ Filecopy.ps1
@@ -2,8 +2,9 @@
 # Define the IP address or hostname to add to the TrustedHosts list
 $trustedHost = "40.112.215.123"
 
-# Modify the TrustedHosts registry key
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WSMAN\Client" -Name "TrustedHosts" -Value $trustedHost -Force
+# Modify the TrustedHosts registry key on the D:\ drive
+Set-ItemProperty -Path "Registry::D:\SOFTWARE\Microsoft\Windows\CurrentVersion\WSMAN\Client" -Name "TrustedHosts" -Value $trustedHost -Force
+
 $userName = "azureuser"
 $password = "codincity@123"
 $secureCred = ConvertTo-SecureString $password -AsPlainText -Force
