ffe7e2fc04454dbfd51d5ef58ce1fc040df1881d
AakashR3 <109940526+AakashR3@users.noreply.github.com>
2023-06-16 20:23:36 +0530

    Update Filecopy.ps1

--- Filecopy.ps1
+++ Filecopy.ps1
@@ -1,14 +1,19 @@
-Set-Item C:\:\\localhost\\Client\\TrustedHosts -Value '40.112.215.123' -Force
 $userName = "azureuser"
 $password = "codincity@123"
 $secureCred = ConvertTo-SecureString $password -AsPlainText -Force
 $credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $userName, $secureCred
-$session = New-PSSession -ComputerName 40.112.215.123 -Credential $credential
+
+# Modify the TrustedHosts list
+$trustedHost = "40.112.215.123"
+Set-Item -Path "wsman:\\localhost\Client\TrustedHosts" -Value $trustedHost -Force
+
+$session = New-PSSession -ComputerName $trustedHost -Credential $credential
 $sourcePath = "$env:WORKSPACE/publish"
 $destinationPath = "C:\Users\azureuser\Desktop\"
-Invoke-Command -Session $session -ArgumentList $sourcePath, $destinationPath -ErrorVariable err_return -ScriptBlock{
-#Copy-Item -Path $sourcePath -Destination $destinationPath
-Copy-Item -Path $args[0] -Destination $args[1]
 
+Invoke-Command -Session $session -ArgumentList $sourcePath, $destinationPath -ErrorVariable err_return -ScriptBlock {
+    # Copy the item from source to destination
+    Copy-Item -Path $args[0] -Destination $args[1]
 }
+
 Remove-PSSession -Session $session
