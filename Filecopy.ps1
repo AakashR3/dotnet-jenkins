11eec2adf38e8e13104ff44a30e6b65119fa052f
AakashR3 <109940526+AakashR3@users.noreply.github.com>
2023-06-17 15:56:14 +0530

    Update Filecopy.ps1

--- Filecopy.ps1
+++ Filecopy.ps1
@@ -1,5 +1,5 @@
 #install gss-ntlmssp
-apt install iputils-ping
+apt-get install iputils-ping
 $userName = "azureuser"
 $password = "codincity@123"
 $secureCred = ConvertTo-SecureString $password -AsPlainText -Force
