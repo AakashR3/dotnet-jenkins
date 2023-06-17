cb48368df876dfae7b19a6511c27a3e68551b89b
AakashR3 <109940526+AakashR3@users.noreply.github.com>
2023-06-17 14:38:43 +0530

    Update Filecopy.ps1

--- Filecopy.ps1
+++ Filecopy.ps1
@@ -5,12 +5,12 @@ $secureCred = ConvertTo-SecureString $password -AsPlainText -Force
 $credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $userName, $secureCred
 
 # Modify the TrustedHosts list
-$trustedHost = "40.112.215.123"
-Set-Item -Path "wsman:\\localhost\Client\TrustedHosts" -Value $trustedHost -Force
+#$trustedHost = "40.112.215.123"
+#Set-Item -Path "wsman:\\localhost\Client\TrustedHosts" -Value $trustedHost -Force
 
 $session = New-PSSession -ComputerName $trustedHost -Credential $credential
 $sourcePath = "$env:WORKSPACE/publish"
-$destinationPath = "C:\Users\azureuser\Desktop\"
+$destinationPath = "D:/"
 
 Invoke-Command -Session $session -ArgumentList $sourcePath, $destinationPath -ErrorVariable err_return -ScriptBlock {
     # Copy the item from source to destination
