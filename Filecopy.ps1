b4dd296ddda99cfe1cae0ce11e629a7201841275
AakashR3 <109940526+AakashR3@users.noreply.github.com>
2023-06-17 16:56:19 +0530

    Update Filecopy.ps1

--- Filecopy.ps1
+++ Filecopy.ps1
@@ -2,11 +2,8 @@ $userName = "azureuser"
 $password = "codincity@123"
 $secureCred = ConvertTo-SecureString $password -AsPlainText -Force
 $credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $userName, $secureCred
-
 $remoteHost = "40.112.215.123"
-
 $session = New-PSSession -ComputerName $remoteHost -Credential $credential -Authentication Negotiate
-
 $sourcePath = "$env:WORKSPACE/publish"
 $destinationPath = "D:/"
 
